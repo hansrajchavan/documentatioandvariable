75c8fc816c3fe8e8fd6b4c982a4d5aacbe2d4316
hansraj <hansrajchavan7@gmail.com>
2025-09-17 18:22:51 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,27 @@
+import sys
+""""
+documentation of demo 
+
+"""
+def demo():
+    pass
+#single line coment
+
+demo()
+
+'''
+multiline comment
+'''
+
+""""
+multiline comment
+
+"""
+print(demo.__doc__)
+print(__doc__)
+
+#vaiable
+def demo1():
+    _abhi=10
+    print(_abhi)
+demo1()    
