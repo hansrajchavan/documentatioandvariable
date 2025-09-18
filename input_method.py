417121a4e3896e57bceb8ca1f1f7a3ff9409d2bd
hansraj <hansrajchavan7@gmail.com>
2025-09-18 18:02:03 +0530

    first commit

---
+++ input_method.py
@@ -0,0 +1,5 @@
+#input method 
+_a =int(input("enter first number"))
+_b =int(input("enter second number"))
+_c =_a+_b
+print(_c)
---
+++ string.py
@@ -0,0 +1,6 @@
+_firstname ="hansraj"
+_lastname ="chavan"
+_age =25
+#f string method
+print(f"my first name is {_firstname} my last name is {_lastname} my age is {_age}")
+
